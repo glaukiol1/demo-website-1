3becc12bc441e19643d5a8043387dc4f61062a38
glaukiol1 <glaukiol1@gmail.com>
2021-05-17 14:23:33 +0200

    Make the title a h2 element

--- src/comp/Home.js
+++ src/comp/Home.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="Home">
       <div className="left-bio">
-        <h1>Hello!</h1>
+        <h2>Hello!</h1>
         <hr />
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis nulla sed imperdiet vulputate. Mauris semper euismod tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc dignissim fringilla semper. Aenean nunc neque, hendrerit sit amet aliquam efficitur, posuere non tortor. Nullam cursus mattis diam ac vehicula. Etiam at ultricies nulla. Proin at rhoncus orci. Morbi molestie nisi volutpat dignissim iaculis. Morbi felis ipsum, sodales eget vulputate sed, faucibus eu felis. </p>
       </div>
